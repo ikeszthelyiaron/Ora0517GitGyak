d4b9bc35f5fef527ed2648405c59ecf47cf07d91
aron <ikeszthelyiaron@gmail.com>
2019-05-17 16:57:06 +0200

    Áron Person Class-a

---
+++ Ora0517GitGyak/src/ora0517gitgyak/Person.java
@@ -0,0 +1,40 @@
+
+package ora0517gitgyak;
+
+
+public class Person {
+    
+    String firstName;
+    String lastName;
+    String name = firstName + lastName;
+    String city;
+    String email;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
