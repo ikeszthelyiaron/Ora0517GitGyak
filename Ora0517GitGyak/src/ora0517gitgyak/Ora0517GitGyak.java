7993b6f4605c673891d46b17d49e7095516b6c70
aron <ikeszthelyiaron@gmail.com>
2019-05-17 14:01:19 +0200

    Branchban metod for cikl kiir

--- Ora0517GitGyak/src/ora0517gitgyak/Ora0517GitGyak.java
+++ Ora0517GitGyak/src/ora0517gitgyak/Ora0517GitGyak.java
@@ -7,6 +7,27 @@ public class Ora0517GitGyak {
     public static void main(String[] args) {
 
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    public void elsoMetodusAzElsoBranchban() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 
 }
