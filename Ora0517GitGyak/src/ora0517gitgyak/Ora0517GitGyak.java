e1b5f7521e0c03837fbb32193caba94c2c505bec
Zsani <zsanettfrona@gmail.com>
2019-05-17 14:56:31 +0200

    beolvasas

--- Ora0517GitGyak/src/ora0517gitgyak/Ora0517GitGyak.java
+++ Ora0517GitGyak/src/ora0517gitgyak/Ora0517GitGyak.java
@@ -1,11 +1,19 @@
 
 package ora0517gitgyak;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 
 public class Ora0517GitGyak {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+//        ArrayList<Person> persons = Reader.read();
+//        for (Person person : persons) {
+//            System.out.println(person);
+//        }
+//        
 
         System.out.println("elsoBranch");
         
